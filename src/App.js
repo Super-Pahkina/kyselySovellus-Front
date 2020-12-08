0c7147a7a62785227f7f923fd3fa75241e060460
Kristiina Kumila <kristiinakumila@gmail.com>
2020-12-08 16:31:34 +0200

    tulossivua siistitty

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import HaeKysely from './components/HaeKysely';
 import Tulosraportti from './components/Tulosraportti';
 //import { Button } from '@material-ui/core';
--- src/components/Tulosraportti.js
+++ src/components/Tulosraportti.js
@@ -1,17 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+const useStyles = makeStyles((theme) => ({
+    Header: {
+        
+    },
+
+/*    Kysely: {
+        position: "-webkit-sticky",
+        position: "sticky",
+        top: 0,
+        backgroundColor: "#FFFFFF"
+    },
+*/
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest
+        })
+    },
+
+    expandOpen: {
+        transform: 'rotate(180deg)'
+    },
+}));
 
 function Tulosraportti(props) {
-
+    const classes = useStyles();
     const [kysely_id, setKysely_id] = useState(props.kysely_id)
     const [data, setData] = useState([])
+    const [expandedId, setExpandedId] = React.useState(-1);
+    const [kysely, setKysely] = useState('');
+    let otsikko = 0;
+    let i = 0;
 
     const [kysymysLista, setKysymyslista] = useState([])
 
     const vastaus_url = `http://kyselysovellus.herokuapp.com/kyselyt/${kysely_id}/vastaukset`
     const kysymys_url = `http://kyselysovellus.herokuapp.com/kyselyt/${kysely_id}/kysymykset`
 
-    useEffect(() => { fetchData() });
+    useEffect(() => { fetchData() }, []);
 
     const fetchData = () => {
 
@@ -20,33 +60,109 @@ function Tulosraportti(props) {
 
         axios.get(kysymys_url)
             .then(response => setKysymyslista(response.data))
+            
+        
         console.log("LISTA", JSON.stringify(kysymysLista[0]))
         console.log("DATA", data)
+        
+        
     }
+
+    const handleChange = (index) => {
+        setExpandedId(expandedId === index ? -1 : index);
+    };
+
 
     return (
         <div>
            <p></p>
-
-            <h2>vastaukset:</h2>
+            
+            
+            <Grid container 
+                direction="column"
+                spacing={1} 
+                className={classes.grid}>
             {kysymysLista.map((kysymys, index) =>
+            <>
+            {otsikko === 0 ? 
+            <Grid item key={ index } className={classes.Kysely}>
+                <h1>{kysymysLista[0].kysely.nimi}</h1>
+                <h2>{kysymysLista[0].kysely.kuvaus}</h2>
+                <h2>vastaukset:</h2>
+                <span style={{display:'none'}}>{otsikko = 1}</span>
+            </Grid>
+            : 
+            <></>
+            }
+            <Grid item key={ index } className={classes.gridItem}>
+                    
                 <div>
-                    <p key={index}><strong>{kysymys.teksti}</strong></p>
+                    <Card>
+                    <CardHeader className={classes.Header}
+                    title={kysymys.teksti}
+                    />
                     {data.map((vastaus, indeksi) => {
-
+                        
                         if (vastaus.kysymys.kysymys_id === kysymys.kysymys_id) {
-                            console.log(vastaus.syote);
+                            i = i + 1;
                             if(vastaus.kysymys.tyyppi === "teksti" || vastaus.kysymys.tyyppi === "radionappula") {
+                                console.log(expandedId)
                                 return (
-                                    <p key={indeksi}>{vastaus.syote}</p>
+                                    <>
+                                        
+                                        {i === 1 ?
+                                        <IconButton className={classes.Icon}
+                                        className={clsx(classes.expand, {
+                                            [classes.expandOpen]: expandedId === index,
+                                            })}
+                                            onClick={() => handleChange(index)}
+                                            aria-expanded={expandedId === index}
+                                        >
+                                            <ExpandMoreIcon />
+                                        </IconButton>
+                                        :
+                                        <></>
+                                        }
+                                            
+                                        <Collapse in={expandedId === index} >
+                                            
+                                        <CardContent className={classes.content}> 
+                                            <Typography key={indeksi}>{i}: {vastaus.syote}</Typography>
+                                        </CardContent>
+                                        </Collapse>
+                                    </>
                                 )
                             } else{
                                 return(
-                                    <div>
+                                    <>
+                                        
+                                        {i === 1 ?
+                                        <IconButton className={classes.Icon}
+                                        className={clsx(classes.expand, {
+                                            [classes.expandOpen]: expandedId === index,
+                                            })}
+                                            onClick={() => handleChange(index)}
+                                            aria-expanded={expandedId === index}
+                                        >
+                                            <ExpandMoreIcon />
+                                        </IconButton>
+                                        :
+                                        <></>
+                                        }
+                                            
+                                        <Collapse in={expandedId === index} >
+                                            
+                                        <CardContent className={classes.content}> 
+                                        <div>
                                         {vastaus.checkbox.map(valinta => 
-                                            <p>{valinta}</p>
+                                        <CardContent className={classes.content}> 
+                                            <Typography>{valinta}</Typography>
+                                        </CardContent>
                                             )}
                                         </div>
+                                        </CardContent>
+                                        </Collapse>
+                                    </>
                                 )
 
                             }
@@ -56,8 +172,14 @@ function Tulosraportti(props) {
                     }
 
                     )}
+                    </Card>
+                    <span style={{display:'none'}}>{i = 0}</span>
                 </div>
+                </Grid>
+                </>
             )}
+            
+            </Grid>
 
         </div>
     )
